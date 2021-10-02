1a1960f7832d3d97c508905dee2138786dea9bee
SJabin <tonni.cse@gmail.com>
2021-10-03 07:21:34 +1100

    Add files via upload

--- model_properties/experiments.py
+++ model_properties/experiments.py
@@ -21,22 +21,30 @@ from models.attack import train_target_model, train_shadow_model, train_attack
 #sampling records 
 
     
-def mia_exec(target_data, shadow_data, shadow_all_indices, layer=1, node=None, lrate=None, l2ratio=None, tgt_model='ANN', sh_model='ANN', save_params = None):
+def mia_exec(target_data, shadow_data, shadow_all_indices, layer=1, node=None, lrate=None, l2ratio=None, tgt_model='ANN', sh_model='ANN', arch = False, save_params = None, mia_ind = False, fairness = False ):
     #train target
     
     # model architecture experiments 
     if node != None:
-        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, n_hidden=node)
+        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, n_hidden=node, arch = arch)
     
     elif lrate != None:
-        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, lrate=lrate)
+        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, lrate=lrate, arch = arch)
     
     elif l2ratio != None:
-        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, l2ratio=l2ratio)
+        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, l2ratio=l2ratio, arch = arch)
         
     # mutual information between records and model parameters
     elif save_params != None:
         attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model, save_params=save_params)
+    
+    # mia_indistingushability 
+    elif mia_ind:
+        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model, mia_ind=mia_ind)
+    
+    # mia_indistingushability 
+    elif fairness:
+        attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model, fairness=fairness)
     
     else:
         #train target
@@ -126,7 +134,7 @@ def get_indices(trsize, shsize, data, exp=None, n_shadow=1):
 def exp_arch(data, trsize, shsize, layer, node=None, lrate=None, l2ratio=None):    
     target_data, shadow_data, shadow_all_indices = get_indices(trsize, shsize, data)
     defaults = get_deafults(target_data)
-    target, atk = mia_exec(target_data, shadow_data, shadow_all_indices, layer = layer, node = node, lrate = lrate, l2ratio = l2ratio)    
+    target, atk = mia_exec(target_data, shadow_data, shadow_all_indices, arch= True, layer = layer, node = node, lrate = lrate, l2ratio = l2ratio)   
     return trsize, shsize, defaults, target, atk
 
 def exp_combination(data, trsize, shsize, tgt_model='ANN', sh_model='ANN'):
@@ -141,48 +149,88 @@ def exp_mutual_info(data, trsize, shsize, save_params):
     target, atk = mia_exec(target_data, shadow_data, shadow_all_indices, save_params=save_params)
     return trsize, shsize, defaults, target, atk
 
+def exp_indistiguishability(data, trsize, shsize):
+    target_data, shadow_data, shadow_all_indices = get_indices(trsize, shsize, data)
+    defaults = get_deafults(target_data)
+    target, atk = mia_exec(target_data, shadow_data, shadow_all_indices, mia_ind = True)    
+    return trsize, shsize, defaults, target, atk
+
+def exp_fairness(data, trsize, shsize):
+    target_data, shadow_data, shadow_all_indices = get_indices(trsize, shsize, data)
+    defaults = get_deafults(target_data)
+    target, atk = mia_exec(target_data, shadow_data, shadow_all_indices, fairness = True)    
+    return trsize, shsize, defaults, target, atk
 
 
-def save_arch_results(itr, result, savefile):
-    trsize, shsize, defaults, target, atk = result
-    fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, arch = target
-    layer, node, lrate, l2ratio = arch
+#exp = ['n_nodes','l_rates', 'l2_ratios', 'combination', 'mutual_info', 'mia_ind', 'fairness']
+def save_results(itr, result, savefile, exp, model = None):
+    trsize, shsize, defaults, target, atk = result    
     atk_prec, atk_rec, atk_acc, class_acc= atk
     
-    result = str(itr)+','+str(layer)+','+str(node)+','+str(lrate)+','+ str(l2ratio)+ ','
-    result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
-    result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
+    result_str = str(itr)+','
     
-    write(result, savefile)
-    
-def save_combination_results(itr, tgt_model, sh_model, result, savefile):
-    trsize, shsize, defaults, target, atk = result
-    
-    if tgt_model =='ANN':
-        fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, _ = target
-    else:
+    if exp == 'n_nodes' or exp == 'l_rates' or exp == 'l2_ratios':
+        fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, arch = target
+        layer, node, lrate, l2ratio = arch
+        result_str += str(layer)+','+str(node)+','+str(lrate)+','+ str(l2ratio)+ ','
+        
+    if exp == 'model_combine':
+        tgt_model, sh_model = model
         fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc = target
-    #layer, node, lrate, l2ratio = arch
-    atk_prec, atk_rec, atk_acc, class_acc= atk
+        result_str += str(tgt_model)+','+str(sh_model)+','
+        
+    if exp == 'mutual_info':
+        fpr,tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc,  avg_mi, max_mi = target
+        result_str += str(avg_mi)+','+str(max_mi)+','
+        
+    if exp == 'mia_ind':
+        fpr,tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc,  indist = target
+        result_str += str(indist)+','
+        
+    if exp == 'fairness':
+        fpr,tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc,  g_fair, p_fair, i_fair = target
+        result_str += str(g_fair)+','+str(p_fair)+','+str(i_fair)+','
     
-    #result = str(itr)+','+str(layer)+','+str(node)+','+str(lrate)+','+ str(l2ratio)+ ','
-    result = str(itr)+','+str(tgt_model)+','+str(sh_model)+','
-    result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
-    result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
+    result_str += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','
+    result_str += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)
     
-    write(result, savefile)
+    write(result_str, savefile)
     
-def save_mi_results(itr, result, savefile):
-    trsize, shsize, defaults, target, atk = result
     
-    fpr,tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, avg_mi, max_mi = target
-    atk_prec, atk_rec, atk_acc, class_acc= atk
+# def save_arch_results(itr, result, savefile):
+#     trsize, shsize, defaults, target, atk = result
+#     fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, arch = target
+#     layer, node, lrate, l2ratio = arch
+#     atk_prec, atk_rec, atk_acc, class_acc= atk
     
-    result = str(itr)+','+str(avg_mi)+','+str(max_mi)+','
-    result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
-    result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
+#     result = str(itr)+','+str(layer)+','+str(node)+','+str(lrate)+','+ str(l2ratio)+ ','
+#     result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
+#     result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
     
-    write(result, savefile)
+#     write(result, savefile)
+    
+# def save_combination_results(itr, tgt_model, sh_model, result, savefile):
+#     trsize, shsize, defaults, target, atk = result
+#     fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc = target
+#     atk_prec, atk_rec, atk_acc, class_acc= atk
+
+#     result = str(itr)+','+str(tgt_model)+','+str(sh_model)+','
+#     result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
+#     result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
+    
+#     write(result, savefile)
+    
+# def save_mi_results(itr, result, savefile):
+#     trsize, shsize, defaults, target, atk = result    
+#     fpr,tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, res = target
+#     avg_mi, max_mi = res
+#     atk_prec, atk_rec, atk_acc, class_acc= atk
+    
+#     result = str(itr)+','+str(avg_mi)+','+str(max_mi)+','
+#     result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
+#     result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
+    
+#     write(result, savefile)
     
 def write(result, savefile):
     text_file=open(savefile, 'a')
@@ -195,8 +243,6 @@ def write(result, savefile):
     
 
 #exp = ['n_nodes','l_rates', 'l2_ratios', 'combination', 'mutual_info', 'mia_ind', 'fairness']
-
-
 def main(datalabel, exp):
     filename='./'+datalabel+'.csv'
     savefile=exp+'_'+datalabel+'.csv'
@@ -218,7 +264,8 @@ def main(datalabel, exp):
                 for node in nodes: 
                     print("nodes per layer: ", node)
                     result = exp_arch(data, trsize, shsize, layer, node=node)
-                    save_arch_results(itr, result, savefile)
+                    #save_arch_results(itr, result, savefile)
+                    save_results(itr, result, savefile, exp)
                 
         if exp == 'l_rates':
             print("\nExp: learning rates============")
@@ -230,7 +277,8 @@ def main(datalabel, exp):
                 for lrate in lrates: 
                     print("learning rate: ", lrate)
                     result = exp_arch(data, trsize, shsize, layer, lrate=lrate)
-                    save_arch_results(itr, result, savefile)
+                    #save_arch_results(itr, result, savefile)
+                    save_results(itr, result, savefile, exp)
             
         if exp == 'l2_ratios':
             print("\nExp: l2 ratios============")
@@ -242,10 +290,11 @@ def main(datalabel, exp):
                 for l2ratio in l2ratios: 
                     print("l2 ratio: ", l2ratio)
                     result = exp_arch(data, trsize, shsize, layer, l2ratio=l2ratio)
-                    save_arch_results(itr, result, savefile)
+                    #save_arch_results(itr, result, savefile)
+                    save_results(itr, result, savefile, exp)
 
         
-        elif exp == 'model_combine':
+        if exp == 'model_combine':
             print("Exp: target - shadow model Combinations============")
             tgt_models = [ 'ANN', 'LR', 'SVC', 'RF', 'KNN']
             sh_models = ['ANN', 'LR', 'SVC', 'RF', 'KNN','All'] 
@@ -255,22 +304,29 @@ def main(datalabel, exp):
                     print("\ntarget model: ", tmodel)
                     print("shadow model: ", smodel)
                     result = exp_combination(data, trsize, shsize, tgt_model=tmodel, sh_model=smodel)
-                    save_combination_results(itr, tmodel, smodel, result, savefile)
+                    models = tmodel, smodel
+                    #save_combination_results(itr, tmodel, smodel, result, savefile)
+                    save_results(itr, result, savefile, exp, model = models)
             
         #I_x : mutual information between the records and the model's parameters
-        elif exp == 'mutual_info':
+        if exp == 'mutual_info':
             save_params = str(itr), savefile
             print("Exp: Mutual Information between the records and model parameters")
             result = exp_mutual_info(data, trsize, shsize, save_params)
-            save_mi_results(itr, result, savefile)
+            #save_mi_results(itr, result, savefile)
+            save_results(itr, result, savefile, exp)
             
-        elif exp == 'mia_ind':
+        if exp == 'mia_ind':
             print("Exp: MIA Indistinguishability============")
+            result = exp_indistiguishability(data, trsize, shsize)
+            #save_mia_results(itr, result, savefile)
+            save_results(itr, result, savefile, exp)
             
-        elif exp == 'fairness':
+        if exp == 'fairness':
             print("Exp: Fairness Difference============")
-        
-               
+            result = exp_fairness(data, trsize, shsize)
+            #save_fairness_results(itr, result, savefile)
+            save_results(itr, result, savefile, exp)  
             
     
    
