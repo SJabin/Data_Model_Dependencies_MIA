35a0d60481f3dc015eaa69814cbb5d52117d740e
SJabin <tonni.cse@gmail.com>
2021-10-08 16:24:37 +1100

    Update experiments.py

--- model_properties/experiments.py
+++ model_properties/experiments.py
@@ -14,13 +14,17 @@ n_shadow=1
 theano.config.optimizer='fast_compile'
 np.random.seed(50215)
 
-
 from utils.data_utils import get_deafults
 from models.attack import train_target_model, train_shadow_model, train_attack
 
-#sampling records 
 
-    
+# Membership Inference Attack 
+# executed in three steps:
+# training the target model -> train_target
+# training the shadow model -> train_shadow, and,
+# run attack -> train_attack 
+# attack accuracy is the test_accuracy of the attack model. 
+
 def mia_exec(target_data, shadow_data, shadow_all_indices, layer=1, node=None, lrate=None, l2ratio=None, tgt_model='ANN', sh_model='ANN', arch = False, save_params = None, mia_ind = False, fairness = False ):
     #train target
     
@@ -33,21 +37,23 @@ def mia_exec(target_data, shadow_data, shadow_all_indices, layer=1, node=None, l
     
     elif l2ratio != None:
         attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, n_layer=layer, l2ratio=l2ratio, arch = arch)
+
         
-    # mutual information between records and model parameters
+    # mutual information between records and model parameters experiment
     elif save_params != None:
         attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model, save_params=save_params)
     
-    # mia_indistingushability 
+    
+    # mia_indistingushability experimnet
     elif mia_ind:
         attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model, mia_ind=mia_ind)
     
-    # mia_indistingushability 
+    # mia_indistingushability experiment
     elif fairness:
         attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model, fairness=fairness)
     
+    
     else:
-        #train target
         attack_test_x, attack_test_y, test_classes, target_results= train_target_model(target_data, model=tgt_model)
 
     #train shadow
@@ -63,62 +69,13 @@ def mia_exec(target_data, shadow_data, shadow_all_indices, layer=1, node=None, l
 
     
 
-def get_indices(trsize, shsize, data, exp=None, n_shadow=1):
-    if exp == 'fairness':
-        if exp == 'class':
-            _size= int(cbal* trsize / 100)
-#             print(_size)
+def get_indices(trsize, shsize, data, n_shadow=1):
+    indices=np.arange(len(data))
+    target_indices = random.sample(indices.tolist(),trsize)
+    shadow_indices = np.setdiff1d(indices, target_indices)
     
-            dt0=data[data['class']==0].reset_index(drop=True)
-            dt1=data[data['class']==1].reset_index(drop=True)
-        
-        if exp == 'feature':
-            _size= int(fbal* trsize / 100)
-
-            
-            dt1=data
-            for i in range(0, 5):
-                dt1=dt1[dt1.iloc[:,i]<=.5]
-            temp_ind=np.setdiff1d(list(data.index), list(dt1.index))
-            dt0= data.iloc[temp_ind,:].reset_index(drop=True)
-            dt1=dt1.reset_index(drop=True)
-
-    
-        indices1=np.arange(len(dt1))
-        indices0=np.arange(len(dt0))      
-        
-        #sample _size records from class = 1  | features <=.5
-        np.random.shuffle(indices1)
-        target_indices = random.sample(indices1.tolist(),_size)
-        shadow_indices = np.setdiff1d(indices1, target_indices)
-        target_d1=dt1.iloc[target_indices,:].reset_index(drop=True)
-        shadow_d1=dt1.iloc[shadow_indices,:].reset_index(drop=True)
-        
-        #sample rest of the records from class = 0 | features >.5
-        np.random.shuffle(indices0)        
-        target_indices = random.sample(indices0.tolist(),trsize-_size)
-        shadow_indices = np.setdiff1d(indices0, target_indices)    
-        target_d0=dt0.iloc[target_indices,:].reset_index(drop=True)
-        shadow_d0=dt0.iloc[shadow_indices,:].reset_index(drop=True)
-                    
-        target_data=pd.concat([target_d0,target_d1]).reset_index(drop=True)       
-        tgt_ind=list(target_data.index)
-        np.random.shuffle(tgt_ind)
-        target_data = target_data.iloc[tgt_ind, :].reset_index(drop=True)
-
-        
-        shadow_data=pd.concat([shadow_d0,shadow_d1]).reset_index(drop=True)
-        shd_ind=list(shadow_data.index)
-        np.random.shuffle(shd_ind)
-        shadow_data = shadow_data.iloc[shd_ind, :].reset_index(drop=True)
-        
-    else:
-        indices=np.arange(len(data))
-        target_indices = random.sample(indices.tolist(),trsize)
-        shadow_indices = np.setdiff1d(indices, target_indices)
-    
-        target_data=data.iloc[target_indices,:].reset_index(drop=True)
-        shadow_data=data.iloc[shadow_indices,:].reset_index(drop=True)
+    target_data=data.iloc[target_indices,:].reset_index(drop=True)
+    shadow_data=data.iloc[shadow_indices,:].reset_index(drop=True)
     
     #indices for multiple shadows.
     shadow_all_indices=[]
@@ -126,7 +83,6 @@ def get_indices(trsize, shsize, data, exp=None, n_shadow=1):
     for j in range(0,n_shadow): shadow_all_indices.append(random.sample(temp.tolist(),shsize)) 
         
     return target_data, shadow_data, shadow_all_indices
-
 
 
 
@@ -162,7 +118,6 @@ def exp_fairness(data, trsize, shsize):
     return trsize, shsize, defaults, target, atk
 
 
-#exp = ['n_nodes','l_rates', 'l2_ratios', 'combination', 'mutual_info', 'mia_ind', 'fairness']
 def save_results(itr, result, savefile, exp, model = None):
     trsize, shsize, defaults, target, atk = result    
     atk_prec, atk_rec, atk_acc, class_acc= atk
@@ -197,40 +152,6 @@ def save_results(itr, result, savefile, exp, model = None):
     write(result_str, savefile)
     
     
-# def save_arch_results(itr, result, savefile):
-#     trsize, shsize, defaults, target, atk = result
-#     fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, arch = target
-#     layer, node, lrate, l2ratio = arch
-#     atk_prec, atk_rec, atk_acc, class_acc= atk
-    
-#     result = str(itr)+','+str(layer)+','+str(node)+','+str(lrate)+','+ str(l2ratio)+ ','
-#     result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
-#     result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
-    
-#     write(result, savefile)
-    
-# def save_combination_results(itr, tgt_model, sh_model, result, savefile):
-#     trsize, shsize, defaults, target, atk = result
-#     fpr, tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc = target
-#     atk_prec, atk_rec, atk_acc, class_acc= atk
-
-#     result = str(itr)+','+str(tgt_model)+','+str(sh_model)+','
-#     result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
-#     result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
-    
-#     write(result, savefile)
-    
-# def save_mi_results(itr, result, savefile):
-#     trsize, shsize, defaults, target, atk = result    
-#     fpr,tpr, roc_auc, ts_prec, ts_rec, ts_fbeta, tr_acc, ts_acc, res = target
-#     avg_mi, max_mi = res
-#     atk_prec, atk_rec, atk_acc, class_acc= atk
-    
-#     result = str(itr)+','+str(avg_mi)+','+str(max_mi)+','
-#     result += str(ts_prec)+','+str(ts_rec)+','+str(tr_acc)+','+str(ts_acc)+','+str(roc_auc)+','            
-#     result += str(atk_prec)+','+str(atk_rec)+','+str(atk_acc)#+','+ str(class_acc[0])+','+ str(class_acc[0])
-    
-#     write(result, savefile)
     
 def write(result, savefile):
     text_file=open(savefile, 'a')
@@ -243,61 +164,74 @@ def write(result, savefile):
     
 
 #exp = ['n_nodes','l_rates', 'l2_ratios', 'combination', 'mutual_info', 'mia_ind', 'fairness']
+
+# Experiments on the n_nodes, l_rates, l2_ratios are performed on ANNs having 1 to 5 hidden layers.
+n_layers = 5
+nodes = [ 5, 50, 100, 500, 1000 ]
+lrates = [ .00001, .0001, .001, .01, .1 ]
+l2ratios = [ 0, .001, .01, .1, 1, 2, 3 ]     
+
+
+# experiments on the different target and shadow model combinations
+# shadow model 'ALL' refers to the classifier build by stacking all five models  
+tgt_models = [ 'ANN', 'LR', 'SVC', 'RF', 'KNN']
+sh_models = ['ANN', 'LR', 'SVC', 'RF', 'KNN','All']
+
+# for all other experiments datasize is fixed. For Adult dataset, datasize =10000
+# for Purchase and Texas datasets change the datasize to 100000
+trsize = shsize = 10000
+
+
+# path for the dataset
+filepath = './'
+
+
+
 def main(datalabel, exp):
-    filename='./'+datalabel+'.csv'
-    savefile=exp+'_'+datalabel+'.csv'
+    filename = filepath + datalabel + '.csv'
+    savefile = exp + '_' + datalabel + '.csv'
     data = pd.read_csv(filename) 
-    trsize = shsize = 10000
+
     
-    for itr in range(0,2):
+    for itr in range(0,100):
         print("\nIteration: ", itr)
         
-        # Experiments on the n_nodes, l_rates, l2_ratios are performed on ANNs having 1 to 5 hidden layers.
         if exp == 'n_nodes':            
             print("\nExp: number of nodes============")
-            nodes = [ 5, 50, 100, 500, 1000 ]
             
             # NN -> 1 to 5 hidden layers
-            for layer in range(1,6):
+            for layer in range(1,n_layers+1):
                 print("\nhidden layers: ", layer)
-                
                 for node in nodes: 
                     print("nodes per layer: ", node)
                     result = exp_arch(data, trsize, shsize, layer, node=node)
-                    #save_arch_results(itr, result, savefile)
                     save_results(itr, result, savefile, exp)
                 
         if exp == 'l_rates':
             print("\nExp: learning rates============")
-            lrates = [ .00001, .0001, .001, .01, .1 ]
-            
+
             # NN -> 1 to 5 hidden layers
-            for layer in range(1,6):
+            for layer in range(1,n_layers+1):
                 print("\nhidden layers: ", layer)
                 for lrate in lrates: 
                     print("learning rate: ", lrate)
                     result = exp_arch(data, trsize, shsize, layer, lrate=lrate)
-                    #save_arch_results(itr, result, savefile)
                     save_results(itr, result, savefile, exp)
             
         if exp == 'l2_ratios':
             print("\nExp: l2 ratios============")
-            l2ratios = [ 0, .001, .01, .1, 1, 2, 3 ]
-            
+                        
             # NN -> 1 to 5 hidden layers
-            for layer in range(1,6):
+            for layer in range(1,n_layers+1):
                 print("\nhidden layers: ", layer)
                 for l2ratio in l2ratios: 
                     print("l2 ratio: ", l2ratio)
                     result = exp_arch(data, trsize, shsize, layer, l2ratio=l2ratio)
-                    #save_arch_results(itr, result, savefile)
                     save_results(itr, result, savefile, exp)
 
         
         if exp == 'model_combine':
             print("Exp: target - shadow model Combinations============")
-            tgt_models = [ 'ANN', 'LR', 'SVC', 'RF', 'KNN']
-            sh_models = ['ANN', 'LR', 'SVC', 'RF', 'KNN','All'] 
             
             for tmodel in tgt_models:
                 for smodel in sh_models:
@@ -305,27 +239,22 @@ def main(datalabel, exp):
                     print("shadow model: ", smodel)
                     result = exp_combination(data, trsize, shsize, tgt_model=tmodel, sh_model=smodel)
                     models = tmodel, smodel
-                    #save_combination_results(itr, tmodel, smodel, result, savefile)
                     save_results(itr, result, savefile, exp, model = models)
             
-        #I_x : mutual information between the records and the model's parameters
         if exp == 'mutual_info':
             save_params = str(itr), savefile
             print("Exp: Mutual Information between the records and model parameters")
             result = exp_mutual_info(data, trsize, shsize, save_params)
-            #save_mi_results(itr, result, savefile)
             save_results(itr, result, savefile, exp)
             
         if exp == 'mia_ind':
             print("Exp: MIA Indistinguishability============")
             result = exp_indistiguishability(data, trsize, shsize)
-            #save_mia_results(itr, result, savefile)
             save_results(itr, result, savefile, exp)
             
         if exp == 'fairness':
             print("Exp: Fairness Difference============")
             result = exp_fairness(data, trsize, shsize)
-            #save_fairness_results(itr, result, savefile)
             save_results(itr, result, savefile, exp)  
             
     
